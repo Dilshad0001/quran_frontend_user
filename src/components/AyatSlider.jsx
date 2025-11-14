c9a6a393209c86a495c587332e2d974f143baea7
Dilshad <dilshadalicp28@gmail.com>
2025-11-14 16:25:10 +0530

    ayat slide direction added

--- src/components/AyatSlider.jsx
+++ src/components/AyatSlider.jsx
@@ -1,11 +1,69 @@
 
+// // import React from "react";
+// // import { Swiper, SwiperSlide } from "swiper/react";
+// // import "swiper/css";
+
+// // function AyatSlider({ ayats, toArabicNumber }) {
+
+// //   // SPLIT AYATS INTO PAGES OF 6
+// //   function chunkAyats(ayats, size) {
+// //     let pages = [];
+// //     for (let i = 0; i < ayats.length; i += size) {
+// //       pages.push(ayats.slice(i, i + size));
+// //     }
+// //     return pages;
+// //   }
+
+// //   const pages = chunkAyats(ayats, 6); // 6 ayats per page
+
+// //   return (
+// //     <Swiper spaceBetween={20} slidesPerView={1}>
+// //       {pages.map((pageAyats, index) => (
+// //         <SwiperSlide key={index}>
+// //         <div
+// //         dir="rtl"
+// //         style={{
+// //             height: "75vh",
+// //             overflow: "hidden",
+// //             fontFamily: "Amiri, serif",
+// //             lineHeight: "3rem",
+// //             padding: "20px",
+// //             backgroundColor: "#faf9f5",
+// //             border: "1px solid #e5e4da",
+// //             borderRadius: "12px",
+            
+// //         }}
+// //         >
+
+// //             {pageAyats.map((a) => (
+// //               <span key={a.id} className="text-3xl">
+// //                 {a.ayat_text}{" "}
+// //                 <span className="inline-flex items-center justify-center w-8 h-8 rounded-full bg-emerald-100 text-emerald-700 text-base mx-2">
+// //                   {toArabicNumber(a.ayat_number)}
+// //                 </span>
+// //               </span>
+// //             ))}
+// //           </div>
+// //         </SwiperSlide>
+// //       ))}
+// //     </Swiper>
+// //   );
+// // }
+
+// // export default AyatSlider;
+
+
 // import React from "react";
 // import { Swiper, SwiperSlide } from "swiper/react";
+// import { Navigation, Pagination } from "swiper/modules";
+
 // import "swiper/css";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
 
 // function AyatSlider({ ayats, toArabicNumber }) {
 
-//   // SPLIT AYATS INTO PAGES OF 6
+//   // split ayats into pages of 6
 //   function chunkAyats(ayats, size) {
 //     let pages = [];
 //     for (let i = 0; i < ayats.length; i += size) {
@@ -14,27 +72,37 @@
 //     return pages;
 //   }
 
-//   const pages = chunkAyats(ayats, 6); // 6 ayats per page
+//   const pages = chunkAyats(ayats, 6);
 
 //   return (
-//     <Swiper spaceBetween={20} slidesPerView={1}>
+//     <Swiper
+//       modules={[Navigation, Pagination]}
+//       spaceBetween={20}
+//       slidesPerView={1}
+//       direction="horizontal"
+//       rtl={true}
+//       navigation
+//       pagination={{ clickable: true }}
+//       style={{ width: "100%", height: "100%" }}
+//     >
 //       {pages.map((pageAyats, index) => (
 //         <SwiperSlide key={index}>
-//         <div
-//         dir="rtl"
-//         style={{
-//             height: "75vh",
-//             overflow: "hidden",
-//             fontFamily: "Amiri, serif",
-//             lineHeight: "3rem",
-//             padding: "20px",
-//             backgroundColor: "#faf9f5",
-//             border: "1px solid #e5e4da",
-//             borderRadius: "12px",
-            
-//         }}
-//         >
-
+//           <div
+//             dir="rtl"
+//             style={{
+//               height: "75vh",
+//               fontFamily: "Amiri, serif",
+//               fontSize: "32px",
+//               lineHeight: "3rem",
+//               lineHeight: "3rem",
+//               padding: "20px",
+//               backgroundColor: "#faf9f5",
+//               border: "1px solid #e5e4da",
+//               borderRadius: "12px",
+//               overflowY: "hidden",
+//               overflowX: "hidden"
+//             }}
+//           >
 //             {pageAyats.map((a) => (
 //               <span key={a.id} className="text-3xl">
 //                 {a.ayat_text}{" "}
@@ -51,19 +119,12 @@
 // }
 
 // export default AyatSlider;
-
-
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
-
 import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 
 function AyatSlider({ ayats, toArabicNumber }) {
 
-  // split ayats into pages of 6
   function chunkAyats(ayats, size) {
     let pages = [];
     for (let i = 0; i < ayats.length; i += size) {
@@ -75,44 +136,42 @@ function AyatSlider({ ayats, toArabicNumber }) {
   const pages = chunkAyats(ayats, 6);
 
   return (
-    <Swiper
-      modules={[Navigation, Pagination]}
-      spaceBetween={20}
-      slidesPerView={1}
-      direction="horizontal"
-      rtl={true}
-      navigation
-      pagination={{ clickable: true }}
-      style={{ width: "100%", height: "100%" }}
-    >
-      {pages.map((pageAyats, index) => (
-        <SwiperSlide key={index}>
-          <div
-            dir="rtl"
-            style={{
-              height: "75vh",
-              fontFamily: "Amiri, serif",
-              lineHeight: "3rem",
-              padding: "20px",
-              backgroundColor: "#faf9f5",
-              border: "1px solid #e5e4da",
-              borderRadius: "12px",
-              overflowY: "hidden",
-              overflowX: "hidden"
-            }}
-          >
-            {pageAyats.map((a) => (
-              <span key={a.id} className="text-3xl">
-                {a.ayat_text}{" "}
-                <span className="inline-flex items-center justify-center w-8 h-8 rounded-full bg-emerald-100 text-emerald-700 text-base mx-2">
-                  {toArabicNumber(a.ayat_number)}
+    <div dir="rtl">
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={1}
+        direction="horizontal"
+        style={{ width: "100%", height: "100%" }}
+      >
+        {pages.map((pageAyats, index) => (
+          <SwiperSlide key={index}>
+            <div
+              dir="rtl"
+              style={{
+                height: "75vh",
+                fontFamily: "Amiri, serif",
+                fontSize: "32px",
+                lineHeight: "3rem",
+                padding: "20px",
+                backgroundColor: "#faf9f5",
+                border: "1px solid #e5e4da",
+                borderRadius: "12px",
+                overflow: "hidden"
+              }}
+            >
+              {pageAyats.map((a) => (
+                <span key={a.id}>
+                  {a.ayat_text}{" "}
+                  <span className="inline-flex items-center justify-center w-8 h-8 rounded-full bg-emerald-100 text-emerald-700 text-base mx-2">
+                    {toArabicNumber(a.ayat_number)}
+                  </span>
                 </span>
-              </span>
-            ))}
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+              ))}
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 }
 
